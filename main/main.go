4b455ed36e8376844b7ad2f736627bbbcdf46e70
Habibi Mustafa <bibitahabibi@gmail.com>
2019-03-25 17:00:28 +0700

    add homepage

--- main/main.go
+++ main/main.go
@@ -1,12 +1,25 @@
 package main
 
 import (
+	"encoding/json"
 	"github.com/gorilla/mux"
 	"log"
 	"net/http"
 )
 
+type Message struct {
+	Success bool
+	Message string
+	Body    interface{}
+}
+
 func main() {
 	router := mux.NewRouter()
+	router.HandleFunc("/", HomePage).Methods("GET")
 	log.Fatal(http.ListenAndServe(":8000", router))
+}
+
+func HomePage(res http.ResponseWriter, req *http.Request) {
+	returnData := Message{true, "Welcome to Olah Citra", nil}
+	_ = json.NewEncoder(res).Encode(returnData)
 }
