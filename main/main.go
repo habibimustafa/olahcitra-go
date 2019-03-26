630994be0a1eb2cdd2419a26ff3285638b736e2d
Habibi Mustafa <bibitahabibi@gmail.com>
2019-03-26 17:40:03 +0700

    returning citra

--- main/main.go
+++ main/main.go
@@ -5,6 +5,7 @@ import (
 	"github.com/gorilla/mux"
 	"log"
 	"net/http"
+	"olahcitra/citra"
 )
 
 type Message struct {
@@ -25,5 +26,24 @@ func HomePage(res http.ResponseWriter, req *http.Request) {
 }
 
 func ToInverse(res http.ResponseWriter, req *http.Request) {
-	_ = json.NewEncoder(res).Encode(req.Body)
+	var jsonParam [][]float64
+
+	decoder := json.NewDecoder(req.Body)
+	if err := decoder.Decode(&jsonParam); err != nil {
+		returnData := Message{false, "Failed decoding data", nil}
+		_ = json.NewEncoder(res).Encode(returnData)
+		return
+	}
+
+	defer req.Body.Close()
+
+	var pixels []citra.Pixel
+	for _, elm := range jsonParam {
+		pixel := citra.Pixel{elm[0], elm[1], elm[2]}
+		_ = append(pixels, pixel)
+	}
+
+	gambar := citra.Citra{Data: pixels}
+
+	_ = json.NewEncoder(res).Encode(gambar)
 }
