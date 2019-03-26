34ba727cd98df34da712e5a1e8c9e50cf7ded096
Habibi Mustafa <bibitahabibi@gmail.com>
2019-03-26 17:46:01 +0700

    test converting citra to inverse

--- citra/citra_test.go
+++ citra/citra_test.go
@@ -22,3 +22,22 @@ func TestCreatingCitra(t *testing.T) {
 	assert.Equal(t, 175., gambar.Data[2].Green)
 	assert.Equal(t, 200., gambar.Data[2].Blue)
 }
+
+func TestConvertCitraToInverse(t *testing.T) {
+	var pixelA = Pixel{Red: 0, Green: 25, Blue: 50}
+	var pixelB = Pixel{Red: 75, Green: 100, Blue: 125}
+	var pixelC = Pixel{Red: 150, Green: 175, Blue: 200}
+	var gambar = Citra{Data: []Pixel{pixelA, pixelB, pixelC}}
+
+	inv := gambar.toInverse()
+	assert.Equal(t, 3, len(inv.Data))
+	assert.Equal(t, 255., inv.Data[0].Red)
+	assert.Equal(t, 230., inv.Data[0].Green)
+	assert.Equal(t, 205., inv.Data[0].Blue)
+	assert.Equal(t, 180., inv.Data[1].Red)
+	assert.Equal(t, 155., inv.Data[1].Green)
+	assert.Equal(t, 130., inv.Data[1].Blue)
+	assert.Equal(t, 105., inv.Data[2].Red)
+	assert.Equal(t, 80., inv.Data[2].Green)
+	assert.Equal(t, 55., inv.Data[2].Blue)
+}
