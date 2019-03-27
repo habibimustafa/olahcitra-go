b3fcda21f8d15c9352cb6d437df282f7ecf081c2
Habibi Mustafa <bibitahabibi@gmail.com>
2019-03-27 18:25:17 +0700

    test inverse image data

--- main/main_test.go
+++ main/main_test.go
@@ -33,7 +33,7 @@ func TestHomePage(t *testing.T) {
 }
 
 func TestToInverse(t *testing.T) {
-	data := [][]int{{1, 2, 3}, {1, 2, 3}, {1, 2, 3}}
+	data := [][]int{{55, 55, 55}, {75, 75, 75}, {125, 125, 125}}
 	jsonData, _ := json.Marshal(&data)
 
 	req, _ := http.NewRequest("POST", "/inverse", bytes.NewBuffer(jsonData))
@@ -42,9 +42,21 @@ func TestToInverse(t *testing.T) {
 
 	assert.Equal(t, 200, res.Code, "Expect OK Response")
 
-	var resBody Message
-	_ = json.Unmarshal(res.Body.Bytes(), &resBody)
+	var result Message
+	_ = json.Unmarshal(res.Body.Bytes(), &result)
 
-	assert.True(t, resBody.Success)
-	assert.Equal(t, "Converting to inverse success", resBody.Message)
+	assert.True(t, result.Success)
+	assert.Equal(t, "Converting to inverse success", result.Message)
+
+	var resBody [][]float64
+	for _, p := range result.Body.([]interface{}) {
+		var pixel []float64
+		for _, q := range p.([]interface{}) {
+			pixel = append(pixel, q.(float64))
+		}
+		resBody = append(resBody, pixel)
+	}
+
+	expectedBody := [][]float64{{200, 200, 200}, {180, 180, 180}, {130, 130, 130}}
+	assert.Equal(t, expectedBody, resBody)
 }
