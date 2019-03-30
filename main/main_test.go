ca82eb80fb9a2a19c8165abc933baea19264f5de
Habibi Mustafa <bibitahabibi@gmail.com>
2019-03-30 11:37:27 +0700

    test route converting img to binary

--- main/main_test.go
+++ main/main_test.go
@@ -91,3 +91,48 @@ func TestToGray(t *testing.T) {
 		}
 	}
 }
+
+func TestToBinary(t *testing.T) {
+	data := [][]int{{0, 25, 50}, {75, 100, 125}, {150, 175, 200}, {225, 250, 255}}
+	jsonData, _ := json.Marshal(&data)
+
+	req, _ := http.NewRequest("POST", "/binary", bytes.NewBuffer(jsonData))
+	res := httptest.NewRecorder()
+	router().ServeHTTP(res, req)
+
+	assert.Equal(t, 200, res.Code, "Expect OK Response")
+
+	var result Message
+	_ = json.Unmarshal(res.Body.Bytes(), &result)
+
+	assert.True(t, result.Success)
+	assert.Equal(t, "Converting to binary success", result.Message)
+
+	var actual [][]float64
+	for _, p := range result.Body.([]interface{}) {
+		var pixel []float64
+		for _, q := range p.([]interface{}) {
+			pixel = append(pixel, q.(float64))
+		}
+		actual = append(actual, pixel)
+	}
+
+	assert.Equal(t, 4, len(actual))
+
+	var expected [][]float64
+	for _, px := range data {
+		var pixel []float64
+		for _, e := range px {
+			ex := e
+			if ex > 128 {
+				ex = 255
+			} else {
+				ex = 0
+			}
+			pixel = append(pixel, float64(ex))
+		}
+		expected = append(expected, pixel)
+	}
+
+	assert.Equal(t, expected, actual)
+}
