193a85943b339722816afbc6a9b83065137b693a
Habibi Mustafa <bibitahabibi@gmail.com>
2019-03-30 10:51:23 +0700

    create router file

---
+++ main/router.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"encoding/json"
+	"github.com/gorilla/mux"
+	"net/http"
+	"olahcitra/citra"
+)
+
+func router() *mux.Router {
+	router := mux.NewRouter()
+	router.HandleFunc("/", HomePage).Methods("GET")
+	router.HandleFunc("/inverse", ToInverse).Methods("POST")
+	return router
+}
+
+func HomePage(res http.ResponseWriter, req *http.Request) {
+	returnData := Message{true, "Welcome to Olah Citra", nil}
+	_ = json.NewEncoder(res).Encode(returnData)
+}
+
+func ToInverse(res http.ResponseWriter, req *http.Request) {
+	var jsonParam [][]float64
+
+	decoder := json.NewDecoder(req.Body)
+	if err := decoder.Decode(&jsonParam); err != nil {
+		returnData := Message{false, "Failed decoding data", nil}
+		_ = json.NewEncoder(res).Encode(returnData)
+		return
+	}
+
+	defer req.Body.Close()
+
+	var pixels []citra.Pixel
+	for _, elm := range jsonParam {
+		pixel := citra.Pixel{Red: elm[0], Green: elm[1], Blue: elm[2]}
+		pixels = append(pixels, pixel)
+	}
+
+	img := citra.Citra{Data: pixels}
+	inv := img.Inverse()
+
+	returnData := Message{true, "Converting to inverse success", inv.Array()}
+	_ = json.NewEncoder(res).Encode(returnData)
+}
