36c6dde5548b9616fd89e46f424f37265e376fb6
Habibi Mustafa <bibitahabibi@gmail.com>
2019-03-30 10:32:39 +0700

    test convert img to binary

--- citra/citra_test.go
+++ citra/citra_test.go
@@ -64,6 +64,32 @@ func TestConvertCitraToGray(t *testing.T) {
 	}
 }
 
+func TestConvertCitraToBinary(t *testing.T) {
+	var pixelA = Pixel{Red: 0, Green: 25, Blue: 50}
+	var pixelB = Pixel{Red: 75, Green: 100, Blue: 125}
+	var pixelC = Pixel{Red: 150, Green: 175, Blue: 200}
+	var pixelD = Pixel{Red: 225, Green: 250, Blue: 255}
+	var img = Citra{Data: []Pixel{pixelA, pixelB, pixelC, pixelD}}
+
+	bin := img.Binary()
+	assert.Equal(t, 4, len(bin.Data))
+
+	for i, px := range img.Data {
+		pxArr := px.Array().([]float64)
+		for j, e := range pxArr {
+			expected := e
+			if e > 128 {
+				e = 255
+			} else {
+				e = 0
+			}
+
+			actual := bin.Data[i].Array().([]float64)[j]
+			assert.Equal(t, expected, actual)
+		}
+	}
+}
+
 func TestCitraToArray(t *testing.T) {
 	var pixelA = Pixel{Red: 0, Green: 25, Blue: 50}
 	var pixelB = Pixel{Red: 75, Green: 100, Blue: 125}
