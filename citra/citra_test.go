ee60cc1977b2361e99e89932bd3372f1f438e427
Habibi Mustafa <bibitahabibi@gmail.com>
2019-03-30 10:34:24 +0700

    formatting

--- citra/citra_test.go
+++ citra/citra_test.go
@@ -9,27 +9,27 @@ func TestCreatingCitra(t *testing.T) {
 	var pixelA = Pixel{Red: 0, Green: 25, Blue: 50}
 	var pixelB = Pixel{Red: 75, Green: 100, Blue: 125}
 	var pixelC = Pixel{Red: 150, Green: 175, Blue: 200}
-	var gambar = Citra{Data: []Pixel{pixelA, pixelB, pixelC}}
+	var img = Citra{Data: []Pixel{pixelA, pixelB, pixelC}}
 
-	assert.Equal(t, 3, len(gambar.Data))
-	assert.Equal(t, 0., gambar.Data[0].Red)
-	assert.Equal(t, 25., gambar.Data[0].Green)
-	assert.Equal(t, 50., gambar.Data[0].Blue)
-	assert.Equal(t, 75., gambar.Data[1].Red)
-	assert.Equal(t, 100., gambar.Data[1].Green)
-	assert.Equal(t, 125., gambar.Data[1].Blue)
-	assert.Equal(t, 150., gambar.Data[2].Red)
-	assert.Equal(t, 175., gambar.Data[2].Green)
-	assert.Equal(t, 200., gambar.Data[2].Blue)
+	assert.Equal(t, 3, len(img.Data))
+	assert.Equal(t, 0., img.Data[0].Red)
+	assert.Equal(t, 25., img.Data[0].Green)
+	assert.Equal(t, 50., img.Data[0].Blue)
+	assert.Equal(t, 75., img.Data[1].Red)
+	assert.Equal(t, 100., img.Data[1].Green)
+	assert.Equal(t, 125., img.Data[1].Blue)
+	assert.Equal(t, 150., img.Data[2].Red)
+	assert.Equal(t, 175., img.Data[2].Green)
+	assert.Equal(t, 200., img.Data[2].Blue)
 }
 
 func TestConvertCitraToInverse(t *testing.T) {
 	var pixelA = Pixel{Red: 0, Green: 25, Blue: 50}
 	var pixelB = Pixel{Red: 75, Green: 100, Blue: 125}
 	var pixelC = Pixel{Red: 150, Green: 175, Blue: 200}
-	var gambar = Citra{Data: []Pixel{pixelA, pixelB, pixelC}}
+	var img = Citra{Data: []Pixel{pixelA, pixelB, pixelC}}
 
-	inv := gambar.Inverse()
+	inv := img.Inverse()
 	assert.Equal(t, 3, len(inv.Data))
 	assert.Equal(t, 255., inv.Data[0].Red)
 	assert.Equal(t, 230., inv.Data[0].Green)
@@ -46,12 +46,12 @@ func TestConvertCitraToGray(t *testing.T) {
 	var pixelA = Pixel{Red: 0, Green: 25, Blue: 50}
 	var pixelB = Pixel{Red: 75, Green: 100, Blue: 125}
 	var pixelC = Pixel{Red: 150, Green: 175, Blue: 200}
-	var gambar = Citra{Data: []Pixel{pixelA, pixelB, pixelC}}
+	var img = Citra{Data: []Pixel{pixelA, pixelB, pixelC}}
 
-	inv := gambar.Gray()
-	assert.Equal(t, 3, len(inv.Data))
+	gray := img.Gray()
+	assert.Equal(t, 3, len(gray.Data))
 
-	for _, pixel := range inv.Data {
+	for _, pixel := range gray.Data {
 		pixelArray := pixel.Array().([]float64)
 		for j := range pixelArray {
 			k := j + 1
