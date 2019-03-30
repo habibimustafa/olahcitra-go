3e308ad7ef650e2d91dd6c52c64685194099d789
Habibi Mustafa <bibitahabibi@gmail.com>
2019-03-30 11:02:14 +0700

    pass test route converting img to gray scale

--- main/router.go
+++ main/router.go
@@ -11,6 +11,7 @@ func router() *mux.Router {
 	router := mux.NewRouter()
 	router.HandleFunc("/", HomePage).Methods("GET")
 	router.HandleFunc("/inverse", ToInverse).Methods("POST")
+	router.HandleFunc("/grayscale", ToGrayScale).Methods("POST")
 	return router
 }
 
@@ -41,5 +42,30 @@ func ToInverse(res http.ResponseWriter, req *http.Request) {
 	inv := img.Inverse()
 
 	returnData := Message{true, "Converting to inverse success", inv.Array()}
+	_ = json.NewEncoder(res).Encode(returnData)
+}
+
+func ToGrayScale(res http.ResponseWriter, req *http.Request) {
+	var jsonParam [][]float64
+
+	decoder := json.NewDecoder(req.Body)
+	if err := decoder.Decode(&jsonParam); err != nil {
+		returnData := Message{false, "Failed decoding data", nil}
+		_ = json.NewEncoder(res).Encode(returnData)
+		return
+	}
+
+	defer req.Body.Close()
+
+	var pixels []citra.Pixel
+	for _, elm := range jsonParam {
+		pixel := citra.Pixel{Red: elm[0], Green: elm[1], Blue: elm[2]}
+		pixels = append(pixels, pixel)
+	}
+
+	img := citra.Citra{Data: pixels}
+	img.Gray()
+
+	returnData := Message{true, "Converting to inverse success", img.Array()}
 	_ = json.NewEncoder(res).Encode(returnData)
 }
