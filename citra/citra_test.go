fd3b2a96fd4fab45e95b517e7e410665437da223
Habibi Mustafa <bibitahabibi@gmail.com>
2019-03-30 10:45:55 +0700

    simplify test

--- citra/citra_test.go
+++ citra/citra_test.go
@@ -28,27 +28,26 @@ func TestConvertCitraToInverse(t *testing.T) {
 	var pixelB = Pixel{Red: 75, Green: 100, Blue: 125}
 	var pixelC = Pixel{Red: 150, Green: 175, Blue: 200}
 	var img = Citra{Data: []Pixel{pixelA, pixelB, pixelC}}
+	var inv = Citra{Data: []Pixel{pixelA, pixelB, pixelC}}
 
-	inv := img.Inverse()
+	inv.Inverse()
 	assert.Equal(t, 3, len(inv.Data))
-	assert.Equal(t, 255., inv.Data[0].Red)
-	assert.Equal(t, 230., inv.Data[0].Green)
-	assert.Equal(t, 205., inv.Data[0].Blue)
-	assert.Equal(t, 180., inv.Data[1].Red)
-	assert.Equal(t, 155., inv.Data[1].Green)
-	assert.Equal(t, 130., inv.Data[1].Blue)
-	assert.Equal(t, 105., inv.Data[2].Red)
-	assert.Equal(t, 80., inv.Data[2].Green)
-	assert.Equal(t, 55., inv.Data[2].Blue)
+
+	for a, c := range inv.Data {
+		for b, actual := range c.Array().([]float64) {
+			expected := 255 - img.Data[a].Array().([]float64)[b]
+			assert.Equal(t, expected, actual)
+		}
+	}
 }
 
 func TestConvertCitraToGray(t *testing.T) {
 	var pixelA = Pixel{Red: 0, Green: 25, Blue: 50}
 	var pixelB = Pixel{Red: 75, Green: 100, Blue: 125}
 	var pixelC = Pixel{Red: 150, Green: 175, Blue: 200}
-	var img = Citra{Data: []Pixel{pixelA, pixelB, pixelC}}
+	var gray = Citra{Data: []Pixel{pixelA, pixelB, pixelC}}
 
-	gray := img.Gray()
+	gray.Gray()
 	assert.Equal(t, 3, len(gray.Data))
 
 	for _, pixel := range gray.Data {
@@ -69,12 +68,12 @@ func TestConvertCitraToBinary(t *testing.T) {
 	var pixelB = Pixel{Red: 75, Green: 100, Blue: 125}
 	var pixelC = Pixel{Red: 150, Green: 175, Blue: 200}
 	var pixelD = Pixel{Red: 225, Green: 250, Blue: 255}
-	var img = Citra{Data: []Pixel{pixelA, pixelB, pixelC, pixelD}}
+	var bin = Citra{Data: []Pixel{pixelA, pixelB, pixelC, pixelD}}
 
-	bin := img.Binary()
+	bin.Binary()
 	assert.Equal(t, 4, len(bin.Data))
 
-	for i, px := range img.Data {
+	for i, px := range bin.Data {
 		pxArr := px.Array().([]float64)
 		for j, e := range pxArr {
 			expected := e
